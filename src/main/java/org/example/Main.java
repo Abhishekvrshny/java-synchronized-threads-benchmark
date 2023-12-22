e2e4c933cb0bcbaa0219a94a94e2e30ed29ba652
Abhishek Varshney <abhishekvrshny@gmail.com>
2023-12-22 16:02:01 +0530

    chore: minor refactor

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,8 +4,8 @@ public class Main {
   public static void main(String[] args) throws InterruptedException {
     System.out.println("Total Available Cores " + Runtime.getRuntime().availableProcessors());
     System.out.println("========================");
-    int[] threadsList = new int[] {2, 4, 8, 11, 13, 16, 23, 25, 32};
-    for (int threads : threadsList) {
+    int[] threadsCountList = new int[] {2, 4, 8, 11, 13, 16, 23, 25, 32};
+    for (int threads : threadsCountList) {
       System.out.println("Thread Count = " + threads);
       run(ThreadType.PLATFORM, threads);
       run(ThreadType.VIRTUAL, threads);
